85824abf6751b5403646b426e514097371c5fa49
Thiago Feldner <tfeldner@hotmail.com>
2023-01-19 20:54:36 -0300

    Criando as rotas das telas, finalizando o layout.

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { Text, View, ScrollView } from "react-native";
+import { useNavigation } from '@react-navigation/native';
 
 import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates';
 
@@ -11,6 +12,9 @@ const minimunSummaryDatesSize = 18 * 5;
 const amountOfDaysToFill = minimunSummaryDatesSize - datesFromYearStart.length;
 
 export function Home() {
+
+  const { navigate} = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -21,7 +25,7 @@ export function Home() {
             <Text 
               key={`${weekDay}-${i}`}
               className="text-zinc-400 text-xl font-bold text-center mx-1"
-              style = {{ width: DAY_SIZE }}
+              style={{ width: DAY_SIZE }}
             >
               {weekDay}
             </Text>
@@ -37,6 +41,7 @@ export function Home() {
           {datesFromYearStart.map(date => (
             <HabitDay 
               key={date.toISOString()}
+              onPress={() => navigate('habit', {date: date.toISOString() })}
             />
             ))
           }
@@ -45,7 +50,8 @@ export function Home() {
             amountOfDaysToFill >0 && Array
             .from({ length: amountOfDaysToFill })
             .map((_, index) => (
-              <View 
+              <View
+                key={index} 
                 className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
                 style={{ width: DAY_SIZE, height: DAY_SIZE }}
               />
