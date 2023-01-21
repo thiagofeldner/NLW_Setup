0ad6e9ce98680fa750467de53033d6c9ba4a4649
Thiago Feldner <tfeldner@hotmail.com>
2023-01-21 20:38:43 -0300

    Finalizado projeto NLW Setup

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -23,10 +23,10 @@ export function SummaryTable() {
   const [summary, setSummary] = useState<Summary>([]);
 
   useEffect(() => {
-    api.get('/summary').then(response => {
+    api.get('summary').then(response => {
       setSummary(response.data)
     })
-  },[]) 
+  }, []) 
 
   return (
     <div className="w-full flex ">
@@ -44,7 +44,7 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map(date => {
+        {summary.length > 0 && summaryDates.map(date => {
           const dayInSummary = summary.find(day => {
             return dayjs(date).isSame(day.date, 'day')
           })
@@ -54,7 +54,7 @@ export function SummaryTable() {
               key={date.toString()} 
               date={date}
               amount={dayInSummary?.amount} 
-              completed={dayInSummary?.completed} 
+              defaultCompleted={dayInSummary?.completed} 
             />
           )
         })}
